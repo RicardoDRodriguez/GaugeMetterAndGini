847fb77af22a192780f20a8a47cb86ab05b8fbeb
RicardoDRodriguez <ricardo.domingos@gmail.com>
2024-01-19 07:24:10 -0300

    Coeficiente GINI calculado com sucesso - Incluida outras tbelas de teste

--- src/DataBase.js
+++ src/DataBase.js
@@ -8,6 +8,7 @@ class DataBase {
 
   static participantes = [
     //sala,id, sequencia, nome, avatar, entradaNaSala, tempoDeFala
+   /*
     new Participante("Sem Interatividade",1, 1, "Pimentel", "avatar", 10, 4928),
     new Participante("Sem Interatividade",2, 1, "Amanda", "avatar", 10, 278),
     new Participante("Sem Interatividade",3, 1, "Sabrina", "avatar", 10, 267),
@@ -27,7 +28,51 @@ class DataBase {
     new Participante("Sem Interatividade",17, 1, "Monica", "avatar", 10, 0),
     new Participante("Sem Interatividade",18, 1, "Monique", "avatar", 10, 0),
     new Participante("Sem Interatividade",19, 1, "Rodrigo", "avatar", 10, 0),
-    new Participante("Sem Interatividade",20, 1, "Silvia", "avatar", 10, 0)
+    new Participante("Sem Interatividade",20, 1, "Silvia", "avatar", 10, 0) 
+    */
+   /*
+    new Participante("Total Interatividade",1, 1, "Pimentel", "avatar", 10, 300),
+    new Participante("Total Interatividade",2, 1, "Amanda", "avatar", 10, 300),
+    new Participante("Total Interatividade",3, 1, "Sabrina", "avatar", 10, 300),
+    new Participante("Total Interatividade",4, 1, "Isa", "avatar", 10, 300),
+    new Participante("Total Interatividade",5, 1, "Patricia", "avatar", 10, 300),
+    new Participante("Total Interatividade",6, 1, "ElianeG", "avatar", 10, 300),
+    new Participante("Total Interatividade",7, 1, "Ana", "avatar", 10, 300),
+    new Participante("Total Interatividade",8, 1, "Ricardo", "avatar", 10, 300),
+    new Participante("Total Interatividade",9, 1, "Mariana", "avatar", 10, 300),
+    new Participante("Total Interatividade",10, 1, "Paulo", "avatar", 10, 300),
+    new Participante("Total Interatividade",11, 1, "Alice", "avatar", 10, 300),
+    new Participante("Total Interatividade",12, 1, "Andreia", "avatar", 10, 300),
+    new Participante("Total Interatividade",13, 1, "ElianeR", "avatar", 10, 300),
+    new Participante("Total Interatividade",14, 1, "Felipe", "avatar", 10, 300),
+    new Participante("Total Interatividade",15, 1, "Isis", "avatar", 10, 300),
+    new Participante("Total Interatividade",16, 1, "Jones", "avatar", 10, 300),
+    new Participante("Total Interatividade",17, 1, "Monica", "avatar", 10, 300),
+    new Participante("Total Interatividade",18, 1, "Monique", "avatar", 10, 300),
+    new Participante("Total Interatividade",19, 1, "Rodrigo", "avatar", 10, 300),
+    new Participante("Total Interatividade",20, 1, "Silvia", "avatar", 10, 300)
+    */
+    new Participante("Media Interatividade",1, 1, "Pimentel", "avatar", 10, 2757),
+    new Participante("Media Interatividade",2, 1, "Amanda", "avatar", 10, 1225),
+    new Participante("Media Interatividade",3, 1, "Sabrina", "avatar", 10, 513),
+    new Participante("Media Interatividade",4, 1, "Isa", "avatar", 10, 458),
+    new Participante("Media Interatividade",5, 1, "Patricia", "avatar", 10, 428),
+    new Participante("Media Interatividade",6, 1, "ElianeG", "avatar", 10, 264),
+    new Participante("Media Interatividade",7, 1, "Ana", "avatar", 10, 260),
+    new Participante("Media Interatividade",8, 1, "Ricardo", "avatar", 10, 258),
+    new Participante("Media Interatividade",9, 1, "Mariana", "avatar", 10, 251),
+    new Participante("Media Interatividade",10, 1, "Paulo", "avatar", 10, 222),
+    new Participante("Media Interatividade",11, 1, "Alice", "avatar", 10, 198),
+    new Participante("Media Interatividade",12, 1, "Andreia", "avatar", 10, 168),
+    new Participante("Media Interatividade",13, 1, "ElianeR", "avatar", 10, 102),
+    new Participante("Media Interatividade",14, 1, "Felipe", "avatar", 10, 60),
+    new Participante("Media Interatividade",15, 1, "Isis", "avatar", 10, 43),
+    new Participante("Media Interatividade",16, 1, "Jones", "avatar", 10, 21),
+    //new Participante("Media Interatividade",17, 1, "Monica", "avatar", 10, 300),
+    new Participante("Media Interatividade",18, 1, "Monique", "avatar", 10, 300),
+    new Participante("Media Interatividade",19, 1, "Rodrigo", "avatar", 10, 300),
+    new Participante("Media Interatividade",20, 1, "Silvia", "avatar", 10, 300)
+    
   ]
   
 
